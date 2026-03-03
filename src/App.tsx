27acefe151e85b3da63d34dcad4c5bd7452e39ca
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-03-03 17:20:23 +0000

    remove routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Router, Route } from 'wouter'
-import { useHashLocation } from 'wouter/use-hash-location'
 import HomePage from 'src/pages/HomePage'
 
 const AppProvider = () => {
-  return (
-    <Router hook={useHashLocation}>
-      <Route path="/" component={HomePage} />
-    </Router>
-  )
+  return <HomePage />
 }
 
 export default AppProvider
