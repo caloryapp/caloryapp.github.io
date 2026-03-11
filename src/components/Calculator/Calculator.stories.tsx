89df9fcd7e21d393ff8d76727fefe44a80e125b8
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-03-11 11:44:05 +0000

    codefix

--- src/components/Calculator/Calculator.stories.tsx
+++ src/components/Calculator/Calculator.stories.tsx
@@ -2,6 +2,7 @@ import { Story, StoryDefault } from '@ladle/react'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '../../i18n'
 import DialogsProvider from '../../providers/DialogsProvider'
+import SettingsProvider from '../../providers/SettingsProvider'
 import StoreProvider from '../../providers/StoreProvider'
 import Calculator from './Calculator'
 
@@ -13,9 +14,11 @@ export const Example: Story = () => {
   return (
     <I18nextProvider i18n={i18n}>
       <DialogsProvider>
-        <StoreProvider>
-          <Calculator />
-        </StoreProvider>
+        <SettingsProvider>
+          <StoreProvider>
+            <Calculator />
+          </StoreProvider>
+        </SettingsProvider>
       </DialogsProvider>
     </I18nextProvider>
   )
